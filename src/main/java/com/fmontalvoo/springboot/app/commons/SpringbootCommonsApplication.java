7dfc53797b93e93076b3f31e75ab8ba403511e72
Frank Montalvo Ochoa <fgmo.dev@fmontalvoo.com>
2022-10-04 23:57:45 -0500

    Initial commit
    Signed-off-by: Frank Montalvo Ochoa <fgmo.dev@fmontalvoo.com>

---
+++ src/main/java/com/fmontalvoo/springboot/app/commons/SpringbootCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.fmontalvoo.springboot.app.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCommonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmontalvoo/springboot/app/commons/SpringbootCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmontalvoo.springboot.app.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
